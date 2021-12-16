96427661a71059b27dfb40fe1e0e2a496b649868
ludy <ludy.farhan.raica.lukita@gmail.com>
2021-12-16 16:34:30 +0700

    add column name

--- models/generated/transactions_model.sql
+++ models/generated/transactions_model.sql
@@ -1,16 +1,16 @@
 {{ config(materialized='incremental') }}
 
 select
-    CAST(id AS string),
-    CAST(user_id AS string),
-    CAST(wallet_tx_id AS string),
-    CAST(amount AS integer),
-    CAST(status AS string),
-    CAST(type AS string),
-    CAST(created_at AS timestamp),
-    CAST(created_by AS string),
-    CAST(updated_at AS timestamp),
-    CAST(updated_at AS string)
+    CAST(id AS string) AS id,
+    CAST(user_id AS string) AS user_id,
+    CAST(wallet_tx_id AS string) AS wallet_tx_id,
+    CAST(amount AS integer) AS amount,
+    CAST(status AS string) AS status,
+    CAST(type AS string) AS type,
+    CAST(created_at AS timestamp) AS created_at,
+    CAST(created_by AS string) AS created_by,
+    CAST(updated_at AS timestamp) AS updated_at,
+    CAST(updated_by AS string) AS updated_by
 from `data-314708.test_referral_v2.transactions`
 
 {% if is_incremental() %}
