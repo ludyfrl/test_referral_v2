e96bbdacce4e8cba2ef69b915f7457194c55afe9
ludy <ludy.farhan.raica.lukita@gmail.com>
2021-12-16 15:15:45 +0700

    add test dbt project

---
+++ models/transactions_model.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='incremental') }}
+
+select
+    CAST(id AS string),
+    CAST(user_id AS string),
+    CAST(wallet_tx_id AS string),
+    CAST(amount AS integer),
+    CAST(status AS string),
+    CAST(type AS string),
+    CAST(created_at AS timestamptz),
+    CAST(created_by AS string),
+    CAST(updated_at AS timestamptz),
+    CAST(updated_at AS string)
+from `data-314708.test_referral_v2.transactions`
+
+{% if is_incremental() %}
+
+    -- this filter will only be applied on an incremental run
+    where updated_at > (select max(updated_at) from {{ this }})
+
+{% endif %}
